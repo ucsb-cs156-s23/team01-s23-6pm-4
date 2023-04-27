bf613b7aba19489eea4c353216f8f0183edeba80
Jessica Ho <shin-yi@umail.ucsb.edu>
2023-04-26 18:57:32 -0700

    Add fixtures for music objects

---
+++ frontend/src/fixtures/musicFixtures.js
@@ -0,0 +1,42 @@
+const musicFixtures = {
+    oneMusic:
+    [
+      {//title, album, artist, genre
+       "id": 1,
+        "title": "Bibo No Aozora",
+        "album": "1996",
+        "artist": "Ryuichi Sakamoto",
+        "genre": "Classical"
+      }
+    ],
+
+    threeMusic:
+    [
+        {
+            "id": 2,
+            "title": "Cupid",
+            "album": "The Beginning",
+            "artist": "FIFTY FIFTY",
+            "genre": "K-pop"
+        },
+
+        {
+            "id": 3,
+            "title": "Daylight",
+            "album": "Daylight",
+            "artist": "David Kushner",
+            "genre": "Pop"
+        },
+
+        {
+            "id": 4,
+            "title": "NUMB",
+            "album": "?",
+            "artist": "XXXTentacion",
+            "genre": "K-pop"
+        },
+        
+    ]
+};
+
+export { musicFixtures };
