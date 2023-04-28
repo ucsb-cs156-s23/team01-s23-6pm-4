674b0065e4451a5c91a5629c3831b9b0051f0b54
Ashley Budman <ashleybudman@umail.ucsb.edu>
2023-04-28 10:25:44 -0700

    ab - add book details

--- frontend/src/tests/pages/Books/BookDetailsPage.test.js
+++ frontend/src/tests/pages/Books/BookDetailsPage.test.js
@@ -1,11 +1,9 @@
-import { render, screen, act, waitFor, fireEvent } from "@testing-library/react";
-import BookEditPage from "main/pages/Books/BookEditPage";
+import { render, screen } from "@testing-library/react";
+import BookDetailsPage from "main/pages/Books/BookDetailsPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
-import mockConsole from "jest-mock-console";
 
 const mockNavigate = jest.fn();
-
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
     useParams: () => ({
@@ -14,12 +12,10 @@ jest.mock('react-router-dom', () => ({
     useNavigate: () => mockNavigate
 }));
 
-const mockUpdate = jest.fn();
 jest.mock('main/utils/bookUtils', () => {
     return {
         __esModule: true,
         bookUtils: {
-            update: (_book) => {return mockUpdate();},
             getById: (_id) => {
                 return {
                     book: {
@@ -35,97 +31,37 @@ jest.mock('main/utils/bookUtils', () => {
     }
 });
 
-
-describe("BookEditPage tests", () => {
+describe("BookDetailsPage tests", () => {
 
     const queryClient = new QueryClient();
-
     test("renders without crashing", () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
-                    <BookEditPage />
+                    <BookDetailsPage />
                 </MemoryRouter>
             </QueryClientProvider>
         );
     });
 
-    test("loads the correct fields", async () => {
-
+    test("loads the correct fields, and no buttons", async () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
-                    <BookEditPage />
+                    <BookDetailsPage />
                 </MemoryRouter>
             </QueryClientProvider>
         );
+        expect(screen.getByText("Fahrenheit 451")).toBeInTheDocument();
+        expect(screen.getByText("Ray Bradbury")).toBeInTheDocument();
+        expect(screen.getByText("A story set in a dystopian society that burns books in order to control the thoughts of its citizens.")).toBeInTheDocument();
+        expect(screen.getByText("Dystopian fiction")).toBeInTheDocument();
 
-        expect(screen.getByTestId("BookForm-title")).toBeInTheDocument();
-        expect(screen.getByDisplayValue('Fahrenheit 451')).toBeInTheDocument();
-        expect(screen.getByDisplayValue('Ray Bradbury')).toBeInTheDocument();
-        expect(screen.getByDisplayValue('A story set in a dystopian society that burns books in order to control the thoughts of its citizens.')).toBeInTheDocument();
-        expect(screen.getByDisplayValue('Dystopian fiction')).toBeInTheDocument();
-    });
-
-    test("redirects to /books on submit", async () => {
-
-        const restoreConsole = mockConsole();
-
-        mockUpdate.mockReturnValue({
-            "book": {
-                id: 3,
-                title: "It Ends with Us",
-                author: "Colleen Hoover",
-                description: "A story about a young woman from an abusive home who struggles to find her way in the world without recreating the patterns of violence from her youth.",
-                genre: "Romance"
-
-
-            }
-        });
-
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <BookEditPage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        )
-
-        const titleInput = screen.getByLabelText("Title");
-        expect(titleInput).toBeInTheDocument();
-
-        const authorInput = screen.getByLabelText("Author");
-        expect(authorInput).toBeInTheDocument();
-
-        const descriptionInput = screen.getByLabelText("Description");
-        expect(descriptionInput).toBeInTheDocument();
-
-        const genreInput = screen.getByLabelText("Genre");
-        expect(genreInput).toBeInTheDocument();
-
-        const updateButton = screen.getByText("Update");
-        expect(updateButton).toBeInTheDocument();
-
-        await act(async () => {
-            fireEvent.change(titleInput, { target: { value: 'It Ends with Us' } })
-            fireEvent.change(authorInput, { target: { value: 'Colleen Hoover' } })
-            fireEvent.change(descriptionInput, { target: { value: 'A story about a young woman from an abusive home who struggles to find her way in the world without recreating the patterns of violence from her youth.' } })
-            fireEvent.change(genreInput, { target: { value: 'Romance' } })
-            fireEvent.click(updateButton);
-        });
-
-        await waitFor(() => expect(mockUpdate).toHaveBeenCalled());
-        await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith("/books"));
-
-        // assert - check that the console.log was called with the expected message
-        expect(console.log).toHaveBeenCalled();
-        const message = console.log.mock.calls[0][0];
-        const expectedMessage =  `updatedBook: {"book":{"id":3,"title":"It Ends with Us","author":"Colleen Hoover","description":"A story about a young woman from an abusive home who struggles to find her way in the world without recreating the patterns of violence from her youth.","genre":"Romance"}`
-
-        expect(message).toMatch(expectedMessage);
-        restoreConsole();
-
+        expect(screen.queryByText("Delete")).not.toBeInTheDocument();
+        expect(screen.queryByText("Edit")).not.toBeInTheDocument();
+        expect(screen.queryByText("Details")).not.toBeInTheDocument();
     });
 
 });
+
 
